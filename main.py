ab741eb7427d9fdb79631faa7e54f03f094cc936
Sareb Janjua <sarebjay@yahoo.com>
2020-06-17 14:01:59 +0300

    Commmmmittttttt!!!!!!!!!!!!!!!!!!!!!!!!

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-data = 0
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
+
+@app.route("/home")
+def homePage():
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
