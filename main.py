07ab5f70ce1adf41b2644b9954e0e3062719aa1e
Sareb Janjua <sarebjay@pop-os.localdomain>
2020-08-03 03:48:43 +0300

    Came back!

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+data = 0
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        valueF = request.form["F"]
+        global data
+        if valueF == "oneF":
+            data += 1
+        elif valueF == "hundredF":
+            data += 100
 
+    print(f"Value: {data}")
+    return render_template("index.html", content=data)
+    
 @app.route("/home")
 def homePage():
     return redirect("/")
